d02d7aedeefc900c5cc6c6f48072b75468d57c2e
ariel-shin <arielshinis@gmail.com>
2017-08-22 17:10:22 -0700

    shortened examples

--- R/imagehistory.R
+++ R/imagehistory.R
@@ -81,12 +81,20 @@ siaction <- R6Class("siaction",
 #' 
 #' web_tiger$undo() # Undoes the brightness addition
 #'
+#'\dontrun{
 #' web_tiger$redo() # Redoes the brightness addition
-#'
+#' 
+#' web_tiger$add_contrast() # Adds contrast
+#' 
+#' web_tiger$add_gamma() #Adds Gamma correction
+#' 
+#' web_tiger$add_rotate() #Adds rotation by 1 degree
+#' 
 #' web_tiger$save('save.si') # Saves the current state. The filename is optional.
 #' 
 #' web_tiger$load('save.si') # Loads from a previously saved state. The filename is optional. 
 #' #Requires a previously instantiated shinyimg instance (argument provided to new can be null).
+#'}
 #'
 #' @section Methods:
 #' \describe{
