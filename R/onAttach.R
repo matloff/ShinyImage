df8d34aa7c85063d50949f9b4ab1eef99f75201a
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2017-09-01 16:00:28 -0700

    for CRAN

--- R/onAttach.R
+++ R/onAttach.R
@@ -6,4 +6,6 @@
     [CC BY 2.0 (http://creativecommons.org/licenses/by/2.0)], 
     via Wikimedia Commons
     ')
+  cat('Warning: This package creates files in your current directory,\n')
+  cat('one file for each version of an image that you create.\n')
 }
